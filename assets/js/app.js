eb71a3a4ad5c1a5ac92786bcc346b5ad777eaaa0
HenRMJ <henryrmejia96@gmail.com>
2022-07-19 04:38:28 -0700

    Finalize functionality
    Finished the functionality for the quote api integration. Got help from other students in the class with this section

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,28 +2,24 @@ const quoteArray = ["test", "test1", "test2", "test3", "test4", "test5", "test6"
 const quote = document.getElementById("quote");
 const url = "https://api.adviceslip.com/advice";
 
+var hi;
+
 const getQuote = async () => {
     try {
         const response = await fetch(url);
         if (response.ok) {
             const quoteData = await response.json();
-            console.log(response);
-            console.log(quoteData);
-            return quoteData.advice.toString();
+            hi = quoteData.slip.advice;
         }
     } catch (error) {
         console.log(error);
     }
 }
 
-
 const newQuote = () => {
-    const randomNum = Math.floor(Math.random() * quoteArray.length);
-
-    quote.innerText = getQuote();
+    getQuote();
+    quote.innerText = hi;
 }
-
-
 
 const button = document.querySelector("button");
 
