0d50e680cc867e9adcf4f60c75a13d93d6414da2
HenRMJ <henryrmejia96@gmail.com>
2022-07-19 05:34:52 -0700

    Final Final functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,26 +2,19 @@ const quoteArray = ["test", "test1", "test2", "test3", "test4", "test5", "test6"
 const quote = document.getElementById("quote");
 const url = "https://api.adviceslip.com/advice";
 
-var hi;
-
 const getQuote = async () => {
     try {
         const response = await fetch(url);
         if (response.ok) {
             const quoteData = await response.json();
-            hi = quoteData.slip.advice;
+            quote.innerText = quoteData.slip.advice;
         }
     } catch (error) {
         console.log(error);
     }
 }
 
-const newQuote = () => {
-    getQuote();
-    quote.innerText = hi;
-}
-
 const button = document.querySelector("button");
 
-button.addEventListener("click", newQuote);
+button.addEventListener("click", getQuote);
 
