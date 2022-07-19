f7a97781ef765dc61c191799d02edaf4e2862046
HenRMJ <henryrmejia96@gmail.com>
2022-07-18 19:06:50 -0700

    Use API

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,26 @@
 const quoteArray = ["test", "test1", "test2", "test3", "test4", "test5", "test6", "test7"];
 const quote = document.getElementById("quote");
+const url = "https://api.adviceslip.com/advice";
+
+const getQuote = async () => {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const quoteData = await response.json();
+            console.log(response);
+            console.log(quoteData);
+            return quoteData.advice.toString();
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 
 const newQuote = () => {
     const randomNum = Math.floor(Math.random() * quoteArray.length);
 
-    quote.innerText = quoteArray[randomNum];
+    quote.innerText = getQuote();
 }
 
 
